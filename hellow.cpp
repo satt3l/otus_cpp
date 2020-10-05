5821f67bcf46a603ab59ff16b7e1f32dab2e7cb8
Anatoly Turubar <anatoly.turubar@inplat.ru>
2020-10-05 11:35:18 +0300

    Homework part 1 - implement CI
    * Create base cmake file which will set version according to passed vars:
      If TRAVIS_TAG exists - it will be used as project_version
      if TRAVIS_TAG not exists but TRAVIS_BUILD present - project_version will be = 0.0.TRAVIS_BUILD
      else project_version == 0.0.1
    * Create hello world application
    * Create base travis-ci file with condition that deploy will be triggered with tags only
    * Create test for version checking and include it in pipeline

---
+++ hellow.cpp
@@ -0,0 +1,7 @@
+#include "lib.h"
+#include <iostream>
+
+int main() {
+  std::cout << "Build number is " << get_version() << std::endl;
+  std::cout << "Hello world!" << std::endl;
+}
---
+++ lib.cpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "version.h"
+#include "lib.h"
+
+int get_version() {
+  return PROJECT_VERSION_PATCH;
+}
